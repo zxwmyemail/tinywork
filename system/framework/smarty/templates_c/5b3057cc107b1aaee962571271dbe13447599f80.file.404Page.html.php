db589dc069c99747c730df5674ef768f70cbb92f
zxwmyemail <zxwmyemail@126.com>
2019-04-18 10:16:28 +0800

    修改404页面

--- system/framework/smarty/templates_c/5b3057cc107b1aaee962571271dbe13447599f80.file.404Page.html.php
+++ system/framework/smarty/templates_c/5b3057cc107b1aaee962571271dbe13447599f80.file.404Page.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2019-04-18 09:45:20
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2019-04-18 10:13:54
          compiled from "D:\phpStudy\PHPTutorial\WWW\tinywork\mvc\view\homeModule\home\404Page.html" */ ?>
 <?php /*%%SmartyHeaderCode:17540123525cb7d6b03dccb6-44044340%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '5b3057cc107b1aaee962571271dbe13447599f80' => 
     array (
       0 => 'D:\\phpStudy\\PHPTutorial\\WWW\\tinywork\\mvc\\view\\homeModule\\home\\404Page.html',
-      1 => 1555468567,
+      1 => 1555553632,
       2 => 'file',
     ),
   ),
@@ -15,48 +15,26 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.21-dev',
   'unifunc' => 'content_5cb7d6b03f12c6_01649208',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_5cb7d6b03f12c6_01649208')) {function content_5cb7d6b03f12c6_01649208($_smarty_tpl) {?><!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>页面丢失了</title>
-		<meta name="viewpoint" content="width=device-width,initial-scale=1">
-		<link rel="stylesheet" href="asset/css/404.css" />
-		<?php echo '<script'; ?>
- src="asset/js/jquery.min.js"><?php echo '</script'; ?>
->
-	</head>
-	<body>
-		<div class="road">
-			<div class="shadow">
-				<div class="shelt">
-					<div class="head">
-						<div class="eyes">
-							<div class="lefteye">
-								<div class="eyeball"></div>
-								<div class="eyebrow"></div>
-							</div>
-							<div class="righteye">
-								<div class="eyeball"></div>
-								<div class="eyebrow"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="hat"></div>
-				<div class="bubble">
-					<a href="index.php">返回首页?</a>
-				</div>
-			</div>
-			<p>404了，你找到的页面不存在！</p>
-		</div>
-	</body>
-	<?php echo '<script'; ?>
- type="text/javascript" src="asset/js/404.js" ><?php echo '</script'; ?>
->
+<?php if ($_valid && !is_callable('content_5cb7d6b03f12c6_01649208')) {function content_5cb7d6b03f12c6_01649208($_smarty_tpl) {?><!DOCTYPE HTML>  
+<html>  
+	<head>  
+		<meta charset="UTF-8" />  
+		<meta name="viewport" content="width=device-width, initial-scale=1">  
+		<meta name="robots" content="none" />  
+		<title>404 Not Found</title>  
+		<link rel="stylesheet" type="text/css" href="asset/css/404.css">  
+	</head>  
+	<body>  
+		<div class="content">   
+			<h1>404</h1>  
+			<h3>大事不妙啦！</h3>  
+			<p>你访问的页面好像不小心被火星人劫走了~</p>  
+			<a href="index.php">返回首页 ></a>  
+		</div>  
+	</body>  
 </html>
 <?php }} ?>
