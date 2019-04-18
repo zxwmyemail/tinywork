5eeadc252bbd9b8978c0a4fbac7742108228352f
zxwmyemail <zxwmyemail@126.com>
2019-04-18 11:30:22 +0800

    修改探针页面显示效果

--- mvc/model/Probe.php
+++ mvc/model/Probe.php
@@ -15,7 +15,9 @@ class Probe {
     }
 
     public function getPhpParam(){
+        $zend_version = zend_version();
         return array(
+            'plugin_zend_version'        => empty($zend_version) ? '<font color=red>×</font>' : $zend_version,
             'php_version'                => PHP_VERSION,
             'php_sapi_name'              => strtoupper(php_sapi_name()),
             'php_memory_limit'           => $this->valueIsOk("memory_limit"),
@@ -25,69 +27,40 @@ class Probe {
             'php_precision'              => $this->valueIsOk("precision"),
             'php_max_execution_time'     => $this->valueIsOk("max_execution_time"),
             'php_default_socket_timeout' => $this->valueIsOk("default_socket_timeout"),
-            'php_doc_root'               => $this->valueIsOk("doc_root"),
-            'php_user_dir'               => $this->valueIsOk("user_dir"),
-            'php_enable_dl'              => $this->valueIsOk("enable_dl"),
-            'php_include_path'           => $this->valueIsOk("include_path"),
             'php_display_errors'         => $this->valueIsOk("display_errors"),
             'php_register_globals'       => $this->valueIsOk("register_globals"),
             'php_magic_quotes_gpc'       => $this->valueIsOk("magic_quotes_gpc"),
             'php_short_open_tag'         => $this->valueIsOk("short_open_tag"),
-            'php_asp_tags'               => $this->valueIsOk("asp_tags"),
-            'php_ignore_repeated_errors' => $this->valueIsOk("ignore_repeated_errors"),
-            'php_ignore_repeated_source' => $this->valueIsOk("ignore_repeated_source"),
             'php_report_memleaks'        => $this->valueIsOk("report_memleaks"),
             'php_magic_quotes_gpc'       => $this->valueIsOk("magic_quotes_gpc"),
             'php_magic_quotes_runtime'   => $this->valueIsOk("magic_quotes_runtime"),
-            'php_allow_url_fopen'        => $this->valueIsOk("allow_url_fopen"),
-            'php_register_argc_argv'     => $this->valueIsOk("register_argc_argv"),
-            'php_cookie'                 => isset($_COOKIE)?'<font color="green">√</font>' : '<font color="red">×</font>',
-            'php_aspell_check_raw'       => $this->isfun("aspell_check_raw"),
             'php_bcadd'                  => $this->isfun("bcadd"),
-            'php_preg_match'             => $this->isfun("preg_match"),
-            'php_pdf_close'              => $this->isfun("pdf_close"),
-            'php_snmpget'                => $this->isfun("snmpget"),
-            'php_vm_adduser'             => $this->isfun("vm_adduser"),
-            'php_curl_init'              => $this->isfun("curl_init"),
-            'php_smtp'                   => get_cfg_var("SMTP")?'<font color="green">√</font>' : '<font color="red">×</font>',
-            'php_smtp_addr'              => get_cfg_var("SMTP")?get_cfg_var("SMTP"):'<font color="red">×</font>',
             'php_disable_functions'      => $this->getDisableFun(),
+            'plugin_allow_url_fopen'     => $this->valueIsOk("allow_url_fopen"),
         );
     }
 
     public function getPluginParam(){
-        $zend_version = zend_version();
-        $plugin_ioncube = '<font color=red>×</font>';
-        if(extension_loaded('ionCube Loader')){   
-            $ys = ioncube_loader_iversion();   
-            $gm = ".".(int)substr($ys,3,2);   
-            $plugin_ioncube = ionCube_Loader_version().$gm;
-        } 
-
         $plugin_gd = '<font color="red">×</font>';
         if(function_exists('gd_info')) {
             $gd_info = @gd_info();
             $plugin_gd = $gd_info["GD Version"];
         }
         return array(
-            'plugin_zend_version'        => empty($zend_version) ? '<font color=red>×</font>' : $zend_version,
-            'plugin_ldap_close'          => $this->isfun("ldap_close"),
             'plugin_eAccelerator'        => (phpversion('eAccelerator'))!='' ? phpversion('eAccelerator') : "<font color=red>×</font>",
-            'plugin_ioncube'             => $plugin_ioncube,
             'plugin_XCache'              => (phpversion('XCache'))!='' ? phpversion('XCache') : "<font color=red>×</font>",
-            'plugin_APC'                 => (phpversion('APC'))!='' ? phpversion('APC') : "<font color=red>×</font>",
             'plugin_ftp_login'           => $this->isfun("ftp_login"),
             'plugin_xml_set_object'      => $this->isfun("xml_set_object"),
             'plugin_session_start'       => $this->isfun("session_start"),
+            'php_cookie'                 => isset($_COOKIE)?'<font color="green">√</font>' : '<font color="red">×</font>',
             'plugin_socket_accept'       => $this->isfun("socket_accept"),
-            'plugin_cal_days_in_month'   => $this->isfun("cal_days_in_month"),
-            'plugin_allow_url_fopen'     => $this->valueIsOk("allow_url_fopen"),
+            'php_smtp'                   => get_cfg_var("SMTP")?'<font color="green">√</font>' : '<font color="red">×</font>',
             'plugin_gd'                  => $plugin_gd,
+            'php_curl_init'              => $this->isfun("curl_init"),
             'plugin_gzclose'             => $this->isfun("gzclose"),
             'plugin_imap_close'          => $this->isfun("imap_close"),
             'plugin_JDToGregorian'       => $this->isfun("JDToGregorian"),
             'plugin_preg_match'          => $this->isfun("preg_match"),
-            'plugin_wddx_add_vars'       => $this->isfun("wddx_add_vars"),
             'plugin_iconv'               => $this->isfun("iconv"),
             'plugin_mb_eregi'            => $this->isfun("mb_eregi"),
             'plugin_mcrypt_cbc'          => $this->isfun("mcrypt_cbc"),
